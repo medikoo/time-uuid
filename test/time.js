64e9560c9d54251639f75282205dd7acd932b456
Mariusz Nowak <mariusz@medikoo.com>
2013-12-06 17:18:57 +0100

    Increment option

--- test/time.js
+++ test/time.js
@@ -1,6 +1,10 @@
 'use strict';
 
 module.exports = function (t, a) {
+	var last;
 	a(typeof t(), 'number', "Type");
 	a.not(t(), t(), "Unique");
+	last = t();
+	a(t.increment(), last + 1, "Increment");
+	a(t() > last + 1, true, "Further");
 };
