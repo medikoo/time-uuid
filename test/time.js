04ac79b76f4e33ec13662aeaca7cf7a41d1cb8d4
Mariusz Nowak <mariusz@medikoo.com>
2013-12-13 16:51:06 +0100

    Guarantee 2 microseconds gap between updates

--- test/time.js
+++ test/time.js
@@ -5,6 +5,7 @@ module.exports = function (t, a) {
 	a(typeof t(), 'number', "Type");
 	a.not(t(), t(), "Unique");
 	last = t();
-	a(t.increment(), last + 1, "Increment");
-	a(t() > last + 1, true, "Further");
+	a(t.increment(), ++last, "Increment");
+	a(t.increment(), ++last, "Increment #2");
+	a(t() > ++last, true, "Further");
 };
